f1517c390f884b16fd347aa2224141077819046e
Elias <elias63856@gmail.com>
2024-09-12 22:18:40 -0300

    correccion en el main

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
+/* 
 import Clases.Particion;
 
-import javax.swing.*;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import  Clases.interfazGrafica;
+*/
+import Clases.interfazGrafica;
+import  javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
